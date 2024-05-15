8c69633ea1dd07a9d41bbd55beff5587a2e8c186
Sairam Pillai <sairam.pillai61@gmail.com>
2024-05-15 15:17:22 +0530

    Expose Celery metrics via Flower UI

--- entrypoint_celery.sh
+++ entrypoint_celery.sh
@@ -2,8 +2,8 @@
 
 # Downloads the tools
 python superagi/tool_manager.py
-
+python -m pip install flower
 # Install dependencies
 ./install_tool_dependencies.sh
 
-exec celery -A superagi.worker worker --beat --loglevel=info
+exec celery -A superagi.worker worker --beat --loglevel=info & celery -A superagi.worker flower
